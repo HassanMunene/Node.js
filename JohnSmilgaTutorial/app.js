d29e2ea3e38edf2147553194fb1caf3f57100b94
HassanMunene <awanzihassan@gmail.com>
2024-01-07 10:11:55 +0300

    we have added the modules topic

---
+++ JohnSmilgaTutorial/app.js
@@ -0,0 +1,7 @@
+const names = require('./names');
+const sayHi = require('./utils');
+
+sayHi(names.john);
+sayHi(names.peter);
+sayHi(names.susan);
+sayHi(names.michael);
---
+++ JohnSmilgaTutorial/names.js
@@ -0,0 +1,10 @@
+// this shoul be local
+const secret = 'SUPER SECRET';
+
+// this will be shareable
+const peter = 'peter';
+const john = 'john';
+const susan = 'susan';
+const michael = 'michael';
+
+module.exports = {peter, john, susan, michael};
