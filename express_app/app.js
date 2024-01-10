abe697655adba070f7dfda39632d5e0a311e23d4
HassanMunene <awanzihassan@gmail.com>
2024-01-10 15:49:28 +0300

    http module

--- express_app/app.js
+++ express_app/app.js
@@ -1,6 +1,11 @@
 const http = require('http');
+const circularJSON = require('circular-json');
 
 http.createServer((request, response) =>{
-    response.end('HELLO THERE');
+    console.log('client hit the server');
+    let req = circularJSON.stringify(request);
+    response.writeHead(200, {'content-type':'text/html'});
+    response.write(`<h1>yooo</h1>`);
+    response.end()
 
 }).listen(5003,()=>console.log('listening...'));
