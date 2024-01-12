43d184db83fb6943a40c6d6f9419f67c31be2503
HassanMunene <awanzihassan@gmail.com>
2024-01-12 14:49:15 +0300

    updating middleware

--- express_app/9-express.js
+++ express_app/9-express.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const app = express();
 
-const logger = (request, response, next) => {
+const middleware = (request, response, next) => {
     const method = request.method;
     const url = request.url;
     const time = new Date();
     console.log(method, url, time);
     next();
 }
-app.use(logger)
-
-app.get('/', (request, response) => {
+app.get('/', middleware, (request, response) => {
     response.send('Home')
 })
-app.get('/about', (request, response) => {
+app.get('/about', middleware, (request, response) => {
     response.send('About');
 })
 
