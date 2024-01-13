b7c2c00304c82d7c6b9102627513fa0beedcd15e
HassanMunene <awanzihassan@gmail.com>
2024-01-13 15:54:54 +0300

    Hello there is the static

--- express_app/app.js
+++ express_app/app.js
@@ -3,9 +3,17 @@ const { people } = require('./data');
 
 const app = express();
 
-app.get('/api/people', (request, response) => {
-    response.status(200);
-    response.json({success: true, data: people});
+app.use(express.static('./methods-public'));
+
+// app.get('/api/people', (request, response) => {
+//     response.status(200);
+//     response.json({success: true, data: people});
+// })
+app.post('/login', (request, response) => {
+    response.send('<h1>Heyy there</h2>')
+})
+app.get('/', (request, response) => {
+    response.send('client connected');
 })
 
 app.listen(5003, () => {
