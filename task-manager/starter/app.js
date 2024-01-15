ab3349487160ca0237df6771b5ee0051a0b843d9
HassanMunene <awanzihassan@gmail.com>
2024-01-15 10:59:33 +0300

    the first route mounting

--- task-manager/starter/app.js
+++ task-manager/starter/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
-app = express();
+const tasksRoute = require('./routes/tasks');
+
+const app = express();
 const port = 5000;
 
+// app.get('/', (req, res)=> {
+//     res.send('Task manager application')
+// })
+app.use('/api/v1/tasks', tasksRoute);
+app.get('/hello', (req, res)=> {
+    res.send('Hello there mate')
+})
+
 app.listen(port, () => {
-    console.log(`Server is listening on port: ${port}...`);
+    'Server listening on port 5000...'
 })
