cefd69c8f4b66a66ed47cdb1377a702da6685126
HassanMunene <awanzihassan@gmail.com>
2024-01-15 09:49:47 +0300

    this is the beginning of the task manager application

---
+++ task-manager/README.md
@@ -0,0 +1,1 @@
+This will be the task manager application
---
+++ task-manager/starter/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+app = express();
+
+app.listen(5000, () => {
+    console.log('app listening on port 5000');
+})
