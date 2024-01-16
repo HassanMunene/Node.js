86dcaebf0eeec82f936aa9453d81ea217c7f13c4
HassanMunene <awanzihassan@gmail.com>
2024-01-16 16:27:12 +0300

    deleting a task in mongo db

--- task-manager/starter/controller/tasks.js
+++ task-manager/starter/controller/tasks.js
@@ -40,8 +40,19 @@ const createTask = async (req, res) => {
 const updateTask = (req, res) => {
     res.send('update task');
 }
-const deleteTask = (req, res) => {
-    res.send('delete a task');
+const deleteTask = async (req, res) => {
+    try {
+        const taskID = req.params.id;
+        const task = await Task.findOneAndDelete({_id: taskID});
+
+        if(task) {
+            return res.status(200).json({msg: 'Task successfully deleted', task: task});
+        } else {
+            return res.status(404).json({msg: `no task found with the id: ${taskID}`});
+        }
+    } catch (error) {
+        res.status(500).json({msg: error});
+    }
 }
 
 module.exports = {
