1540b1e8a6ab3e289eeef8775613cbfcbfa04c4f
HassanMunene <awanzihassan@gmail.com>
2024-01-19 20:26:02 +0300

    connected the application to the database

--- store_api/app.js
+++ store_api/app.js
@@ -1,7 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const notFoundMiddleware = require('./middleware/not_found');
+const errorHandlerMiddleware = require('./middleware/error_handler');
+const connectDB = require('./db/connect_db');
+const port = process.env.PORT || 5000
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello there</h2>')
+    res.send('<h1>STORE API</h1>')
 })
-app.listen(5000, console.log('Server listening on port 5000'));
+
+// products routes
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+    try {
+        // connect to the db
+        app.listen(port, console.log(`Server listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
