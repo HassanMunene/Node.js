4fe284814e735fbf01a920bd205de9f8cf0fb999
HassanMunene <awanzihassan@gmail.com>
2024-01-20 11:09:02 +0300

    setting up products route

--- store_api/app.js
+++ store_api/app.js
@@ -4,7 +4,10 @@ const app = express();
 const notFoundMiddleware = require('./middleware/not_found');
 const errorHandlerMiddleware = require('./middleware/error_handler');
 const connectDB = require('./db/connect_db');
+const productsRouter = require('./routes/productsRoute');
+
 const port = process.env.PORT || 5000
+const url = process.env.MONGO_URI2
 
 app.use(express.json());
 
@@ -12,6 +15,7 @@ app.get('/', (req, res) => {
     res.send('<h1>STORE API</h1>')
 })
 
+app.use('/api/v1/products', productsRouter);
 // products routes
 
 app.use(notFoundMiddleware);
@@ -20,6 +24,7 @@ app.use(errorHandlerMiddleware);
 const start = async () => {
     try {
         // connect to the db
+        await connectDB(url);
         app.listen(port, console.log(`Server listening on port ${port}...`));
     } catch (error) {
         console.log(error);
