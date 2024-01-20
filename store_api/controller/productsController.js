a141befbb819d2f805b1745fdf229877b89c201a
HassanMunene <awanzihassan@gmail.com>
2024-01-20 20:24:44 +0300

    added the limit query parameter

--- store_api/controller/productsController.js
+++ store_api/controller/productsController.js
@@ -1,13 +1,13 @@
 const Product = require('../models/productModel');
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find().select('name price');
+    const products = await Product.find().select('name price').limit(5);
 
     res.status(200).json({products: products, nbHits: products.length});
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort, field } = req.query;
+    const { featured, company, name, sort, field, limit } = req.query;
     const queryObject = {};
 
     if (featured) {
@@ -31,6 +31,12 @@ const getAllProducts = async (req, res) => {
         let fieldList = field.split(',').join(' ')
         //console.log(fieldList);
         result = result.select(fieldList);
+    }
+    if (limit) {
+        console.log(limit);
+        result = result.limit(limit);
+    } else {
+        result = result.limit(10);
     }
     const products = await result;
     res.status(200).json({products: products, nbHits: products.length});
