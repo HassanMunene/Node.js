12c16eb5f68b8ba279e42ea9f7d2be2911d0fb7b
HassanMunene <awanzihassan@gmail.com>
2024-01-25 17:13:14 +0300

    Im starting to figure out this error thing

---
+++ Jobs-Api/errors/cutom-api-error.js
@@ -0,0 +1,10 @@
+class CustomApiError extends Error {
+    constructor(message, statusCode) {
+        super(message)
+        this.statusCode = statusCode;
+        this.message = message;
+        this.stack = Error.captureStackTrace;
+        this.status = statusCode >=400 && statusCode < 500 ? 'fail': 'Server error'
+    }
+}
+module.exports = CustomApiError;
